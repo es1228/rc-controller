fedd299d56fbb97b8ac380cb82ef1d596ec3d644
Evan Sidhu <129010227+es1228@users.noreply.github.com>
2026-05-29 18:18:04 -0400

    disable text selection button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 
 const Button = ({ icon, text, onClick }: ButtonProps) => {
 	return (
-		<div className="group hover:bg-primary bg-on-bg-light dark:bg-on-bg-dark flex items-center justify-center gap-2 rounded-full p-4 hover:cursor-pointer" onClick={onClick}>
+		<div className="group hover:bg-primary bg-on-bg-light dark:bg-on-bg-dark flex items-center justify-center gap-2 rounded-full p-4 hover:cursor-pointer select-none" onClick={onClick}>
 			<span className="icon icon-rounded group-hover:icon-700 group-hover:cursor-pointer group-hover:transition-all">
 				{icon}
 			</span>
